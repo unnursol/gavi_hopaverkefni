5d0c1a27cd6b9d30fa38dac172cbe8489acab17e
Johanna Svovudottir <johannasvovudottir@gmail.com>
2016-12-12 17:47:03 +0000

    Python code for insertstatements and insertstatements

--- sunAndMoon.py
+++ sunAndMoon.py
@@ -1,3 +1,18 @@
 import csv
 
 file = open('moon-phases-1970-2015-America_New_York.csv')
+
+dataStoreage = csv.DictReader(file) 
+
+data = []
+for x in dataStoreage:
+	data.append(x)
+
+file.close()
+
+outfile = open('insertstatements.sql', 'w')
+
+for i in data:
+	outfile.write("insert into moons (phase, time) values ('{}', {});\n".format(i['phase'], i['timestamp']))
+
+outfile.close()
