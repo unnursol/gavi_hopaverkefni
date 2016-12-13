615827f9f701f8093bef1e3c8a08ffc274245bef
Unnur Sól Ingimarsdóttir <unnuri15@ru.is>
2016-12-13 23:08:47 +0000

    crimes

--- Data/crimes.py
+++ Data/crimes.py
@@ -1,6 +1,35 @@
 import csv
 import datetime
-file = open('crime_homicide_subset.csv')
+import psycopg2
+import getpass
+import time
+
+def connect_to_database(host, dbname, username, pw):
+    conn_string = "host='{}' dbname='{}' user='{}' password='{}'".format(host, dbname, username, pw)
+
+    try:
+        conn = psycopg2.connect(conn_string)
+    except psycopg2.OperationalError as e:
+        print('Connection failed!')
+        print('Error message:', e)
+        exit()
+
+    cursor = conn.cursor()
+
+    print("Connected!\n")
+
+    return cursor, conn
+
+cursor, conn = connect_to_database('localhost','moondata','postgres','postgres')
+
+
+numberofrowstoinsert = 2000
+counter = 0
+
+insertstring = "insert into crimes (time, offense) values "
+values = []
+
+file = open('Crimes_-_2001_to_present.csv')
 
 dataStoreage = csv.DictReader(file) 
 
@@ -10,9 +39,15 @@ for x in dataStoreage:
 
 file.close()
 
-outfile = open('insertstatementsForCrimes.sql', 'w')
 
 for i in data:
-	outfile.write("insert into crimes (time, offense, method) values ({}, '{}', '{}');\n".format(int(datetime.datetime.strptime(i['REPORT_DAT'], "%m/%d/%Y %H:%M").timestamp()), i['OFFENSE'], i['METHOD'] ))
+    values.append((int(datetime.datetime.strptime(i['time'], "%m/%d/%Y %I:%M:%S %p").timestamp()), i['offense']))
 
-outfile.close()
+args_str = b','.join(cursor.mogrify("(%s,%s)", x) for x in values)
+cursor.execute(insertstring + args_str.decode('utf-8'))
+
+
+conn.commit()
+cursor.close()
+conn.close()
+
