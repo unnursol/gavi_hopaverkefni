6cba5723f61d58cdc298641b3fc8bbeab6bd60b8
Unnur Sól Ingimarsdóttir <Unnur Sl Ingimarsdttir>
2016-12-13 15:30:08 +0000

    emergency calls^^

--- 911.py
+++ 911.py
@@ -13,6 +13,6 @@ file.close()
 outfile = open('insertstatementsFor911Calls.sql', 'w')
 
 for i in data:
-    outfile.write("insert into crimes (time, address) values ({}, '{}');\n".format(int(datetime.datetime.strptime(i['timeStamp'], "%m/%d/%Y %H:%M").timestamp()), i['addr'] ))
+    outfile.write("insert into emergencyCalls (time, address) values ({}, '{}');\n".format(int(datetime.datetime.strptime(i['timeStamp'], "%m/%d/%Y %H:%M").timestamp()), i['addr'] ))
 
 outfile.close()
--- sunAndMoonCreateTable.sql
+++ sunAndMoonCreateTable.sql
@@ -1,6 +1,21 @@
 create table moons (
 	id serial,
 	phase varchar (250),
-	time timestamp, 
+	time timestamp,
 	primary key (id)
-)
+);
+
+create table crimes (
+ id serial,
+ time int,
+ offense varchar(50),
+ method varchar (50),
+ primary key (id)
+);
+
+create table emergencyCalls (
+	id serial,
+	time int,
+	address varchar(250),
+	primary key (id)
+);
