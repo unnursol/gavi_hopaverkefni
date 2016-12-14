74076b042e362393bf16e24eb60f1527f299602a
Unnur Sól Ingimarsdóttir <unnuri15@ru.is>
2016-12-14 00:56:37 +0000

    connection file

---
+++ DataAccess/connection.py
@@ -0,0 +1,29 @@
+import psycopg2
+import getpass
+
+host = 'localhost'
+dbname = input('Database name: ')
+
+username = input('User name for {}.{}: '.format(host,dbname))
+pw = getpass.getpass()
+
+conn_string = "host='{}' dbname='{}' user='{}' password='{}'".format(host, dbname, username, pw)
+
+print("Connecting to database {}.{} as {}".format(host, dbname, username))
+
+try:
+    conn = psycopg2.connect(conn_string)
+except psycopg2.OperationalError as e:
+    print('Connection failed!')
+    print('Error message:', e)
+    exit()
+
+cursor = conn.cursor()
+
+print("Connected!\n")
+
+
+# Do something here...
+
+cursor.close()
+conn.close()
